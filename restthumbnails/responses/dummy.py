9e5975b794e9b6461266a54923b7e35cb076ab23
Henrique Carvalho Alves <hcarvalhoalves@gmail.com>
2012-11-28 18:59:12 -0200

    Bugfix: dummy backend

--- restthumbnails/responses/dummy.py
+++ restthumbnails/responses/dummy.py
@@ -4,5 +4,5 @@ from django.views.static import serve
 
 def sendfile(request, location, **kwargs):
     from restthumbnails import defaults
-    storage = defaults.storage_backend
+    storage = defaults.storage_backend()
     return serve(request, location, storage.location)
